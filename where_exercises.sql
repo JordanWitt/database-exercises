fa0907182f43fea82fa574d3c00a829d7621f455
JordanWitt <wittjordan94@gmail.com>
2022-12-07 14:19:56 -0600

    re-adding, everything deleted when pushed. HO HO HO

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,1 +1,32 @@
 USE employees;
+
+SELECT emp_no, first_name
+FROM employees
+WHERE first_name IN ('Irene', 'Vidya', 'Maya');
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT first_name, gender
+FROM employees
+WHERE (first_name IN ('Irene')
+OR first_name IN ('Vidya')
+OR first_name IN ('Maya'))
+AND gender = 'm';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%' OR last_name NOT LIKE '%qu%';

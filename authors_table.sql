866fbebadc87d3ceccf2b4366436784f09a68802
JordanWitt <wittjordan94@gmail.com>
2022-12-12 11:25:56 -0600

    indexes done

--- authors_table.sql
+++ authors_table.sql
@@ -8,3 +8,19 @@ UNIQUE (first_name, last_name)
 );
 
 DESCRIBE authors;
+ALTER TABLE authors
+ADD UNIQUE (first_name, last_name);
+DESCRIBE authors;
+SELECT * FROM authors;
+INSERT INTO authors (first_name, last_name)
+VALUES ('John', 'Steinbeck');
+SELECT * FROM authors;
+
+
+ALTER TABLE authors
+ADD UNIQUE (first_name, last_name);
+DESCRIBE authors;
+SELECT * FROM authors;
+# INSERT INTO authors (first_name, last_name)
+# VALUES ('John', 'Steinbeck');
+# CANT INSERT DUPLICATE UNIQUE VALUE

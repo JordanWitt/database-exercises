db29a59da68e3c0f08c94e5dc3c5658a4e5c55d9
JordanWitt <wittjordan94@gmail.com>
2022-12-13 14:02:20 -0600

    created a datatbase design for discord users

--- database_design.sql
+++ database_design.sql
@@ -19,7 +19,7 @@ CREATE TABLE roles (
     content TEXT NOT NULL,
     users_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (users_id) REFERENCES users (id)
+    FOREIGN KEY (users_id) REFERENCES users(id)
 );
 
 INSERT INTO roles (users_id, content)
@@ -36,3 +36,34 @@ DESCRIBE users;
 DESCRIBE roles;
 SELECT * FROM users;
 SELECT * FROM roles;
+
+CREATE TABLE responsibility (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO responsibilities(name)
+VALUES
+    ('Kick Member'),
+    ('Ban Member'),
+    ('Create Invite'),
+    ('Edit Username'),
+    ('Edit Channels');
+
+CREATE TABLE user_responsibility(
+     user_id INTEGER UNSIGNED NOT NULL ,
+     roles_id INTEGER UNSIGNED NOT NULL,
+     responsibility_id INTEGER UNSIGNED NOT NULL,
+     FOREIGN KEY (user_id) REFERENCES users(id),
+     FOREIGN KEY (roles_id) REFERENCES roles(id),
+     FOREIGN KEY (responsibility_id) REFERENCES responsibilities(id)
+);
+
+INSERT INTO user_responsibility(user_id, roles_id, responsibility_id)
+VALUES
+(1,1,1), (1,1,2), (1,1,3), (1,1,4), (1,1,5),
+(2,2,1), (2,2,2), (2,2,3), (2,2,4),
+(3,3,3), (3,3,4);
+
+
